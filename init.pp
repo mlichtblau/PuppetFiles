e0e2645ffc3d20e0fbfc35da1995165ce6082252
hkohlsaat <hkohlsaat@gmail.com>
2019-02-04 14:10:47 +0000

    Create init.pp

---
+++ init.pp
@@ -0,0 +1,31 @@
+class accounts {
+  
+  @accounts::virtual { 'Ad.Min':
+    uid             =>  4000,
+    realname        =>  'Ad Min',
+    sshkeytype      =>  'ssh-rsa',
+    sshkey          =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQCTUTAF3yfXN747+Lx2JnGWza/zG0M9zgAiPFrVIOEF4j6vtbQ4jlDGEqnpY1lWzsxXp60foaynJGx3DhMYbffI77EIYYJAGqaRuxpxEhctAIbXqtHBSIgnZNT9uCieXgIZvbbY/QfoEsxHcMAsnFeLHC39uG4mgiB54hzHuEWs/FXcd9rjphe/APF1NKiukRUkCxUbVVVneqDWy6IMbhsnhzI1IYlEPl9KX0nhqjv1LaIFv7TZhxJOMEMaJzOoan5p1zCp8mQbABBvdrFHhiJTd3Hsy5+pmBzGjqjJUo7cl8SauV8BxnK1mAfJh76QfVOXwbLUGxEzv7hUVgC2XRmp'
+  }
+
+  @accounts::virtual { 'Max.Mustermann':
+    uid             =>  4001,
+    realname        =>  'Max Mustermann',
+    sshkeytype      =>  'ssh-rsa',
+    sshkey          =>  ''
+  }
+
+  @accounts::virtual { 'Wissenschaftlicher.Mitarbeiter':
+    uid             =>  4002,
+    realname        =>  'Wissenschaftlicher Mitarbeiter',
+    sshkeytype      =>  'ssh-rsa',
+    sshkey          =>  ''
+  }
+
+  @accounts::virtual { 'Ad.Min':
+    uid             =>  4003,
+    realname        =>  'Ad Min',
+    sshkeytype      =>  'ssh-rsa',
+    sshkey          =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQCjiqa9OoAvz3jb1ONnYb5P4j9axUAcUMs5GuZ/Ts6RS8Pz5WEdvaknJYCowLTD43O0eElbJWAiulnI6FcbMUEJwHfDObgRMdKcDrolpj2dSyFakgn85dOY0GdQXlrsZqRAQUJH18jeYPy2NCYPBFdUNsMIa77OvTHlO2gl9XOZQMEktomffh2R0gxkqJ6iZpG1M1IbY38aBr21NC7bCcKF4O9yFwMEfxuO1NEdoWCsBtmYmlAeXMN1lLh6ZuM59wXmpic+DKUZ/1HYw4zRJnwE8EiTDG3vPmftwyCohMXuS+cyWFs6/0CpqvG8Shax0SWG6hLUW5kyyftocoNSp2N7'
+  }
+
+}
