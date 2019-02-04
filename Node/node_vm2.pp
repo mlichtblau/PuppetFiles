824d609dfcc9505eed0ff8f6f8917030032916f7
Hannes Kohlsaat <hkohlsaat@gmail.com>
2019-02-04 15:52:57 +0100

    Delete node_vm2.pp

--- Node/node_vm2.pp
+++
@@ -1,7 +0,0 @@
-class node_vm2 {
-        $admins = ["Ad.Min", "Max.Mustermann"]
-        $users = ["Wissenschaftlicher.Mitarbeiter"]
-
-        realize(Accounts::Virtual[$admins], Accounts::Sudoroot[$admins])
-        realize(Accounts::Virtual[$users])
-}
