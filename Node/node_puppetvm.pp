0b574216348635a7a53315fef48e5efd347cac07
Hannes Kohlsaat <hkohlsaat@gmail.com>
2019-02-04 15:52:50 +0100

    Delete node_puppetvm.pp

--- Node/node_puppetvm.pp
+++
@@ -1,7 +0,0 @@
-class node_puppetvm {
-        $admins = ["Ad.Min"]
-        $users = ["Wissenschaftlicher.Mitarbeiter"]
-
-        realize(Accounts::Virtual[$admins], Accounts::Sudoroot[$admins])
-        realize(Accounts::Virtual[$users])
-}
