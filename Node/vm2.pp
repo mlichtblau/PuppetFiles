177807df6cd883f9101acbfa4a5ac8640eadaf08
Hannes Kohlsaat <hkohlsaat@gmail.com>
2019-02-04 16:06:18 +0100

    Delete vm2.pp

--- Node/vm2.pp
+++
@@ -1,7 +0,0 @@
-class node_vm2 {
-        $admins = ["Ad.Min"]
-        $users = []
-
-        realize(Accounts::Virtual[$admins], Accounts::Sudoroot[$admins])
-        realize(Accounts::Virtual[$users])
-}
