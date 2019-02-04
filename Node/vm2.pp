3a11d2f9c07969ff12f987fb6867a9c6b9579695
hkohlsaat <hkohlsaat@gmail.com>
2019-02-04 14:35:31 +0000

    Add vm2

---
+++ Node/vm2.pp
@@ -0,0 +1,7 @@
+class node_vm2 {
+        $admins = ["Ad.Min"]
+        $users = []
+
+        realize(Accounts::Virtual[$admins], Accounts::Sudoroot[$admins])
+        realize(Accounts::Virtual[$users])
+}
